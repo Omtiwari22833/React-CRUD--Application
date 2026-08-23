183b58c1ccb75bee102ab31636b924046507112e
omtiwari22833 <om177145@gmail.com>
2026-08-23 20:48:09 +0530

    compl

--- springboot-backend/src/main/java/net/javaguides/springboot/config/DBDemo.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/config/DBDemo.java
@@ -4,14 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBDemo {
+
     public static void main(String[] args) {
-        String JdbcURL = "jdbc:mysql://localhost:3306/employee_management_system?useSSL=false";
-        String Username = "root";
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_management_system?useSSL=false";
+        String username = "root";
         String password = "Arjun22833";
-        Connection con = null;
+
         try {
-            con = DriverManager.getConnection(JdbcURL, Username, password);
-            System.out.println("Connected to MySQL database");
+            Connection con = DriverManager.getConnection(
+                    jdbcURL,
+                    username,
+                    password
+            );
+
+            System.out.println("Connected to MySQL database successfully!");
+
+            con.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
